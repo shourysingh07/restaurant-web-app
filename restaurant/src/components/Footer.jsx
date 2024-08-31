6c4af6d61cfad8b83774964bf4b4e8a1651c1a4e
shourysingh07 <shourysingh007@gmail.com>
2024-08-31 15:56:01 +0530

    first_commit

---
+++ restaurant/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="p-4 bg-slate-900 text-center text-white font-semibold">
+      <h2>
+        Made by{" "}
+        <a
+          href="https://www.linkedin.com/in/shoury2007/"
+          className="hover:text-red-600 hover:underline hover:underline-offset-8 "
+        >
+          Shoury Singh
+        </a>
+      </h2>
+    </div>
+  );
+};
+
+export default Footer;
